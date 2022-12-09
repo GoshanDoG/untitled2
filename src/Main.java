45904d1faa0a7fe794b96ef3badcb8d4abe74ec3
admin <zhevrakov.ra.ki25@.ru>
2022-12-09 14:38:36 +0300

    jj

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        QuadraticEquations quadraticEquations = new QuadraticEquations();
-        quadraticEquations.equationRoots();
-    }
-}
--- src/SquareError.java
+++
@@ -1,5 +0,0 @@
-public class SquareError extends RuntimeException {
-  public   SquareError(String massing ){
-      super(massing);
-  }
-}
